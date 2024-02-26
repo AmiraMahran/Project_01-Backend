bddb56d286493260f7654176915130ebac71aae3
Amira Mohamed <amiramahran321@gmail.com>
2024-02-26 09:51:14 +0200

    Deal with people's data

---
+++ js/AllData.js
@@ -0,0 +1,12 @@
+const firstName = "Aira"
+const lastName = "mohamed"
+const city = "Cairo"
+const age = "20"
+
+
+module.exports = {
+    fName: firstName,
+    lName: lastName,
+    city: city,
+    age: age,
+}
